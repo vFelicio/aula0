c0b439e26c6ef58f61d1b21450e1bd23f899d148
Victor Felicio Lima - 81711978 <81711978@animaedu.intranet>
2020-02-21 21:50:35 -0300

    versao inicial

---
+++ Aluno.java
@@ -0,0 +1,17 @@
+public class Aluno{
+private String nome;
+private int id;
+
+public Aluno(String nome, int id){
+this.nome = nome;
+this.id = id;
+}
+
+public String getNome(){
+return this.nome;
+}
+
+public int getId(){
+return this.id;
+}
+}
